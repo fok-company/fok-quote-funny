a1cc78b734dcd3130e2ca836fab6c6ee318b1a01
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-06-19 21:42:59 +0000

    Add some quotes

--- configuration.nix
+++ configuration.nix
@@ -3,11 +3,14 @@
     ["Hey mate can you boost fascism in Sweden real quick?" "Hetman Foko"]
     ["What fascism? that's communism my friend! Oh wait no" "Alpaka Mocy"]
     ["I am about to quote you hold on" "Hetman Foko"]
-    ["(also use ∪ to clone your parents)" "Poollover Nathan"]
+    ["(also use ∪ to clone your parents)" "PoolloverNathan"]
     ["so u dump ur shiet into an src folder and it shits shit" "GNamimates"]
     ["I have slaves that will parse http requests for me" "Hetman Foko"]
     ["give me a quote" "Cheeselover Foko"]
-
+    ["i'm murdering tall mens" "Spingot"]
+    ["nah i don't want to be polymorphed into ohitselectric rn", "PoolloverNathan"]
+    ["why do we have grenades in the tailnet", "Hetman Foko"]
+    ["Yeah and if we catch you posting stuff without a link/zip folder I will break your legs", "Electric"]
   ];
   plush = [
     "                              ████████"
